a7b8ac0d4b8689475d9f57d8c32dfc3413d20927
rexthonyy <rexthonyy@gmail.com>
2021-01-18 15:42:39 +0100

    Access Control Allow Origin in server.js middleware

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('/favicon.ico', express.static('public/images/icons/appicon.png'));
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
+
 const signupRoute = require("./routes/signup");
 app.use("/signup", signupRoute);
 
